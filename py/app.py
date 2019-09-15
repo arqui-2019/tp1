f8c4b1eb688fc7bcc92d9e7ad2e70e9a0a173568
Gustavo adrian Gimenez <gustavo.gimenez@mercadolibre.com>
2019-09-15 17:36:13 -0300

    Python server

---
+++ py/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from time import sleep
+app = Flask(__name__)
+
+@app.route("/ping")
+def ping():
+    return "pong"
+
+@app.route("/timeout")
+def timeout():
+    sleep(5)
+    return "Ok", 200
+    
+@app.route("/intensive")
+def intensive():
+    fact = 1
+    for i in range(1,90):
+        fact = fact * i
+    return "Ok", 200
+
+if __name__ == "__main__":
+    app.run()
